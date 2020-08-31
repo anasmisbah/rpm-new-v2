2ad8ff383f592480ab870bd9c30cc93c7c406cea
Anas Misbahuddin <anas.misbahuddin4869@gmail.com>
2020-08-31 13:16:55 +0800

    change respon user me driver

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-
+use App\Http\Resources\DeliveryOrder as DeliveryOrderResource;
 class UserController extends Controller
 {
     public function me()
@@ -26,13 +26,28 @@ class UserController extends Controller
             return response()->json($user, 200);
         }else {
             $user->driver->avatar = url('/uploads/' . $user->driver->avatar);
+            $user->driver->agen->logo = url('/uploads/' . $user->driver->agen->logo);
             $user->role;
-            $user->driver->agen->logo= url('/uploads/' . $user->driver->agen->logo);
+            $user->driver->delivery_order;
+            foreach ($user->driver->delivery_order as $key => $delivery_order) {
+                $user->driver->delivery_order[$key] = new DeliveryOrderResource($delivery_order);
+            }
+            $data['user']=$user;
 
-            // TODO HISTORY DELIVERY
-            $user->delivery_orders = $user->driver->delivery_order()->where('status',2)->get();
+            $agen = $user->driver->agen;
+            $route = $user->driver->route;
+            $sales_orders = $agen->sales_orders;
 
-            return response()->json($user, 200);
+            $delivery_orders = [];
+            foreach ($sales_orders as $sales_order) {
+                foreach ($sales_order->delivery_orders as $delivery_order) {
+                    if (($route == $delivery_order->shipped_via || $delivery_order->shipped_via == 2) && $delivery_order->status == 0) {
+                        $delivery_orders[] = new DeliveryOrderResource($delivery_order);
+                    }
+                }
+            }
+            $data['ready_delivery_order']=$delivery_orders;
+            return response()->json($data, 200);
         }
     }
 }
