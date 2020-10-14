2f0a5db711e480076489f274997aed86cc3566e9
Anas Misbahuddin <anas.misbahuddin4869@gmail.com>
2020-10-14 12:17:14 +0800

    api coupon response

--- app/Http/Controllers/API/CouponController.php
+++ app/Http/Controllers/API/CouponController.php
@@ -13,13 +13,12 @@ class CouponController extends Controller
         public function index()
         {
             $customer = Auth::user()->customer;
-            $coupons = $customer->coupons()->orderBy('created_at','desc')->get();
+            $coupons = $customer->sales_orders()->orderBy('created_at','desc')->get();
             $data = [];
             foreach ($coupons as $key => $coupon) {
                 $data[] = [
                     "id"=> $coupon->id,
-                    "code_coupon"=> $coupon->code_coupon,
-                    "customer_id"=> $coupon->customer_id,
+                    "code_coupon"=> $coupon->sales_order_number,
                     'created_at'=>$coupon->created_at->format('d F Y'),
                     "updated_at"=> $coupon->updated_at->format('d F Y'),
                 ];
